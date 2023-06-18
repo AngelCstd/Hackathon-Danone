29a31b646e70d752f6b8b55b62a80b7a9a1df6f0
AngelCstd <luisacast.29@gmail.com>
2023-06-17 22:50:32 -0600

    Iniciando

---
+++ hackathon-danone/src/components/header/Header.jsx
@@ -0,0 +1,20 @@
+import "./header.css";
+
+export const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            {/* <Link></Link> */}
+          </li>
+        </ul>
+      </nav>
+      <img src="./ image3.png" alt="logo" />
+      <div>
+        <img src="" alt="" />
+        <span>User</span>
+      </div>
+    </header>
+  );
+};
